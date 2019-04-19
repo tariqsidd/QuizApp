9dd5918604dae7c8586851c3602d34a8914bba76
Syed Amir Ali <sumair.sagitarius@gmail.com>
2019-04-19 19:16:41 +0500

    first commit

--- app/src/main/java/com/example/quizapp/Model/Question.java
+++ app/src/main/java/com/example/quizapp/Model/Question.java
@@ -20,7 +20,7 @@ public class Question {
         AnswerB = answerB;
         AnswerC = answerC;
         AnswerD = answerD;
-        CorrectAnswer = correcktAnswer;
+        CorrectAnswer = correctAnswer;
     }
 
     public int getID() {
